9ce19c11be031b842edc87104dca225a3b4802ac
Corey Vickroy Jr <cjvickroy@gmail.com>
2023-01-07 19:10:01 -0800

    Added first MLB functionality

--- server.js
+++ server.js
@@ -61,7 +61,65 @@ function getTeam(team, res){
     })
 }
 
-function getAllGames(date, res){
+function getAllGamesMLB(date, res){
+    var url = "https://www.espn.com/mlb/scoreboard/_/date/" + date;
+    
+    var day = date.substring(6, 8);
+    var month = date.substring(4, 6);
+    var year = date.substring(0, 4);
+
+    var allGames = [];
+
+    //Use Cheerio to scrape data from the URL
+    request(url, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            $('.Scoreboard__Column--Score').each(function (i) {
+                var game = [];
+                var homeTeam = $(this).find('.ScoreboardScoreCell__Item--home .ScoreCell__TeamName').text();
+                var awayTeam = $(this).find('.ScoreboardScoreCell__Item--away .ScoreCell__TeamName').text();
+                var homeScore = $(this).find('.ScoreboardScoreCell__Item--home .ScoreboardScoreCell_Linescores > div:nth-child(1)').text();
+                var awayScore = $(this).find('.ScoreboardScoreCell__Item--away .ScoreboardScoreCell_Linescores > div:nth-child(1)').text();
+                var gameTime = $(this).parents('.Scoreboard__Row').children('.Scoreboard__Column--2').find('.LastPlay h1').text();
+                var homeRecord = $(this).find('.ScoreboardScoreCell__Item--home .ScoreboardScoreCell__Record:nth-child(1)').text();
+                var awayRecord = $(this).find('.ScoreboardScoreCell__Item--away .ScoreboardScoreCell__Record:nth-child(1)').text();
+                var homeHits = $(this).find('.ScoreboardScoreCell__Item--home .ScoreboardScoreCell_Linescores > div:nth-child(2)').text();
+                var awayHits = $(this).find('.ScoreboardScoreCell__Item--away .ScoreboardScoreCell_Linescores > div:nth-child(2)').text();
+                var homeErrors = $(this).find('.ScoreboardScoreCell__Item--home .ScoreboardScoreCell_Linescores > div:nth-child(3)').text();
+                var awayErrors = $(this).find('.ScoreboardScoreCell__Item--away .ScoreboardScoreCell_Linescores > div:nth-child(3)').text();
+                
+                game.push({
+                    home: homeTeam,
+                    away: awayTeam,
+                    homeScore: homeScore,
+                    awayScore: awayScore,
+                    time: gameTime,
+                    homeRecord: homeRecord,
+                    awayRecord: awayRecord,
+                    homeHits: homeHits,
+                    awayHits: awayHits,
+                    homeErrors: homeErrors,
+                    awayErrors: awayErrors
+                });
+
+                //If the game is over, change time to "Final"
+                if ((homeScore != "" && awayScore != "") && (gameTime == "")){
+                    game[0].time = "Final";
+                }
+
+                allGames.push(game);
+            });
+            
+            res.status(200).send({
+                date: month + "/" + day + "/" + year,
+                numberOfGames: allGames.length,
+                games: allGames
+            });
+        }
+    });
+}
+
+function getAllGamesNBA(date, res){
     var url = "https://www.espn.com/nba/scoreboard/_/date/" + date;
 
     var day = date.substring(6, 8);
@@ -101,10 +159,14 @@ function getAllGames(date, res){
                     awayLogo: awayLogo
                 });
 
+                //If the game is over, change time to "Final"
+                if ((homeScore != "" && awayScore != "") && (gameTime == "")){
+                    game[0].time = "Final";
+                }
 
                 allGames.push(game);
                 
-            });  
+            });
 
             res.status(200).send({
                 date: month + "/" + day + "/" + year,
@@ -152,7 +214,7 @@ app.get('/nba/games/today', function (req, res, next) {
 
     var date = year + month + day;
 
-    getAllGames(date, res);
+    getAllGamesNBA(date, res);
 
 })
 
@@ -165,7 +227,7 @@ app.get('/nba/games/tomorrow', function (req, res, next) {
     
     var date = year + month + day;
 
-    getAllGames(date, res);
+    getAllGamesNBA(date, res);
 })
 
 app.get('nba/teams/:team', function (req, res, next) {
@@ -181,6 +243,19 @@ app.get('nba/teams/:team', function (req, res, next) {
     getTeam(team, res);  
 })
 
+
+
+app.get('/mlb/games/:date', function (req, res, next) {
+
+    let date = req.params.date;
+    let year = date.substring(0, 4);
+    let month = date.substring(4, 6);
+    let day = date.substring(6, 8);
+
+    getAllGamesMLB(date, res);
+})
+    
+   
 //Send all NBA games on a specific date. Date format: YYYYMMDD
 app.get('/nba/games/:date', function (req, res, next) {
 
